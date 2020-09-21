70cb7d408a9c9362d3896ca27aa00b47aeb59c4a
Andy <andyjara.guitar.7@gmail.com>
2020-09-21 19:29:10 -0300

    agregué un form1.html y código js

--- form1.js
+++ form1.js
@@ -1,1 +1,10 @@
 
+var contador = 0;
+function incrementar(){
+    contador++;
+    alert('El contador vale: '+contador);
+}
+function resetear(){
+    contador=0;
+    alert('El contador reseteado ahora vale: '+contador);
+}
